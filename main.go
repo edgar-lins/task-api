bdb4c5d425e589d7fc6572cbc70134e715926778
Edgar Lins <linss.edgar@gmail.com>
2026-01-22 18:56:50 -0300

    Learn: Transformando em uma API (JSON)

--- main.go
+++ main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-// Esse função é um "Handler". Ela lida com quem bate na porta do servidor.
-// w = Writer (onde escrevemos a resposta para o usuário)
-// r = Request (os dados que o usuário mandou, tipo IP, navegador, etc)
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bem-vindo ao meu primeiro Backend em Go! 🚀")
-	fmt.Println("Alguém acessou a home page!") // Isso aparece no seu terminal
+type Task struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	// Simulando dados que viriam do banco de dados
+	tasks := []Task{
+		{ID: 1, Description: "Aprender HTTP", Done: true},
+		{ID: 2, Description: "Criar primeira API", Done: false},
+	}
+
+	// Define que a resposta é do tipo JSON (importante para o navegador entender)
+	w.Header().Set("Content-Type", "application/json")
+
+	// Transforma a struct JSON e envia para o navegador (w)
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func main() {
-	// Roteamento: Quando alguém acessar "/", chame a função homePage
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/tasks", getTasks) // Mudamos a rota para /tasks
 
-	fmt.Println("Servidor rodando na porta 8080...")
-	// Liga o servidor na porta 8080
-	// O 'nil' significa que usaremos o roteador padrão do Go
+	println("Servidor rodando em http://localhost:8080/tasks")
 	http.ListenAndServe(":8080", nil)
 }
